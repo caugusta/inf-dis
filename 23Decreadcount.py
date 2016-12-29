40a4bf2bb2fbb3556342ab4c5ab0e9decfbf7275
caugusta@uoguelph.ca <caugusta@GPU1>
2016-12-29 11:27:37 -0500

    edited 23Decreadcount.py to work with the new version of the epidemic files

--- 23Decreadcount.py
+++ 23Decreadcount.py
@@ -9,11 +9,61 @@ import sys
 import numpy as np
 
 #neighbourhood epidemics
-with open( 'epidemics_neighbourhood_23Dec.json', 'r' ) as f :
+
+#extras
+
+with open( 'epidemics_neighbourhood_Dec23_redo.json', 'r' ) as f :
     data = f.read()
 
-count = len(data.split('}{')) #9600 as desired
-print count
+count = len(data.split('}, {')) #should be 800
+print 'redo neighbourhood epidemics', count
+str_epi = ' '.join(data.split('{'))
+str_epi2 = str_epi.split('}')
+#print str_epi2[0] #first epidemic
+
+times = re.findall(r'"(.\w*)"', str_epi2[0])
+infindivs = re.findall(r':(.\w*),', str_epi2[0]) #need this to also return the last element of the string
+lastone = str_epi2[0][-1]
+new1 = infindivs
+new1.append(lastone)
+
+
+times = np.asarray(times, dtype=np.int32)
+infindivs = np.asarray(infindivs, dtype=np.int32)
+newinf = infindivs
+
+i=1
+while i < len(str_epi2):
+        #print i
+        times_1 = re.findall(r'"(.\w*)"', str_epi2[i])
+        infindivs_1 = re.findall(r':(.\w*),', str_epi2[i])
+        lastone = str_epi2[0][-1]
+        newinf = infindivs_1
+        newinf.append(lastone)
+        if len(newinf) < 2:
+                print 'list was empty'
+                i += 1
+                continue
+        else:
+                times = np.concatenate((times, np.asarray(times_1, dtype=np.int32)))
+                #newinf = np.concatenate((newinf, np.asarray(newinf_1, dtype=np.int32)))
+                infindivs = np.concatenate((infindivs, np.asarray(infindivs_1, dtype=np.int32)))
+                i += 1
+
+
+with open('Dec29_neighbourhood_epidemics_redo.txt', 'w') as f:
+        np.savetxt(f, np.c_[times, infindivs], fmt=['%i', '%i'])
+
+print 'wrote Dec29_neighbourhood_epidemics_redo.txt'
+
+
+#original
+with open( 'epidemics_neighbourhood_Dec23.json', 'r' ) as f :
+    data = f.read()
+
+count = len(data.split('}, {'))
+#count = len(data.split('}{')) #should be 8800
+print 'original neighbourhood epidemics', count
 
 str_epi = ' '.join(data.split('{'))
 str_epi2 = str_epi.split('}')
@@ -32,7 +82,7 @@ newinf = infindivs
 
 i=1
 while i < len(str_epi2):
-	print i
+	#print i
 	times_1 = re.findall(r'"(.\w*)"', str_epi2[i])
 	infindivs_1 = re.findall(r':(.\w*),', str_epi2[i])
 	lastone = str_epi2[0][-1]
@@ -54,19 +104,20 @@ while i < len(str_epi2):
 # 2  4
 # 3  9
 
-with open('Dec23_neighbourhood_epidemics.txt', 'w') as f:
+with open('Dec29_neighbourhood_epidemics.txt', 'w') as f:
 	np.savetxt(f, np.c_[times, infindivs], fmt=['%i', '%i'])
 
-print 'wrote Dec23_neighbourhood_epidemics.txt'
+print 'wrote Dec29_neighbourhood_epidemics.txt'
 
 
 ##Exponential
 
-with open( 'epidemics_exponential_23Dec.json', 'r' ) as f :
+with open( 'epidemics_exponential_Dec23.json', 'r' ) as f :
     data = f.read()
 
-count = len(data.split('}{')) #9600 as desired
-print count
+count = len(data.split('}, {'))
+#count = len(data.split('}{')) #9600 as desired
+print 'exponential epidemics', count
 
 str_epi = ' '.join(data.split('{'))
 str_epi2 = str_epi.split('}')
@@ -85,7 +136,7 @@ newinf = infindivs
 
 i=1
 while i < len(str_epi2):
-        print i
+        #print i
         times_1 = re.findall(r'"(.\w*)"', str_epi2[i])
         infindivs_1 = re.findall(r':(.\w*),', str_epi2[i])
         lastone = str_epi2[0][-1]
@@ -101,18 +152,19 @@ while i < len(str_epi2):
                 i += 1
 
 
-with open('Dec23_exponential_epidemics.txt', 'w') as f:
+with open('Dec29_exponential_epidemics.txt', 'w') as f:
         np.savetxt(f, np.c_[times, infindivs], fmt=['%i', '%i'])
 
-print 'wrote Dec23_exponential_epidemics.txt'
+print 'wrote Dec29_exponential_epidemics.txt'
 
 ##Powerlaw
 
-with open( 'epidemics_powerlaw_23Dec.json', 'r' ) as f :
+with open( 'epidemics_powerlaw_Dec23.json', 'r' ) as f :
     data = f.read()
 
-count = len(data.split('}{')) #9600 as desired
-print count
+count = len(data.split('}, {'))
+#count = len(data.split('}{')) #9600 as desired
+print 'powerlaw epidemics', count
 
 str_epi = ' '.join(data.split('{'))
 str_epi2 = str_epi.split('}')
@@ -131,7 +183,7 @@ newinf = infindivs
 
 i=1
 while i < len(str_epi2):
-        print i
+        #print i
         times_1 = re.findall(r'"(.\w*)"', str_epi2[i])
         infindivs_1 = re.findall(r':(.\w*),', str_epi2[i])
         lastone = str_epi2[0][-1]
@@ -147,7 +199,7 @@ while i < len(str_epi2):
                 i += 1
 
 
-with open('Dec23_powerlaw_epidemics.txt', 'w') as f:
+with open('Dec29_powerlaw_epidemics.txt', 'w') as f:
         np.savetxt(f, np.c_[times, infindivs], fmt=['%i', '%i'])
 
-print 'wrote Dec23_powerlaw_epidemics.txt'
+print 'wrote Dec29_powerlaw_epidemics.txt'
