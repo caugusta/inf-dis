e96e5fb20109f7636d2c4a1b97a479849e6bbdb3
caugusta@uoguelph.ca <caugusta@GPU1>
2016-12-30 12:45:58 -0500

    converted epidemics to R readable format

--- 29Decreadcount.py
+++ 29Decreadcount.py
@@ -10,7 +10,51 @@ import numpy as np
 
 #neighbourhood epidemics
 
-#extras
+with open( 'epidemics_neighbourhood_Dec29_redo.json', 'r' ) as f :
+    data = f.read()
+
+count = len(data.split('}, {')) #should be 800
+print 'neighbourhood epidemics -redo', count
+str_epi = ' '.join(data.split('{'))
+str_epi2 = str_epi.split('}')
+#print str_epi2[0] #first epidemic
+
+times = re.findall(r'"(.\w*)"', str_epi2[0])
+infindivs = re.findall(r':(.\w*),', str_epi2[0]) #need this to also return the last element of the string
+lastone = str_epi2[0][-1]
+new1 = infindivs
+new1.append(lastone)
+
+
+times = np.asarray(times, dtype=np.int32)
+infindivs = np.asarray(infindivs, dtype=np.int32)
+newinf = infindivs
+
+i=1
+while i < len(str_epi2):
+        #print i
+        times_1 = re.findall(r'"(.\w*)"', str_epi2[i])
+        infindivs_1 = re.findall(r':(.\w*),', str_epi2[i])
+        lastone = str_epi2[0][-1]
+        newinf = infindivs_1
+        newinf.append(lastone)
+        if len(newinf) < 2:
+                print 'list was empty'
+                i += 1
+                continue
+        else:
+                times = np.concatenate((times, np.asarray(times_1, dtype=np.int32)))
+                #newinf = np.concatenate((newinf, np.asarray(newinf_1, dtype=np.int32)))
+                infindivs = np.concatenate((infindivs, np.asarray(infindivs_1, dtype=np.int32)))
+                i += 1
+
+
+with open('Dec29_neighbourhood_epidemics_redo.txt', 'w') as f:
+        np.savetxt(f, np.c_[times, infindivs], fmt=['%i', '%i'])
+
+print 'wrote Dec29_neighbourhood_epidemics_redo.txt' 
+
+#original
 
 with open( 'epidemics_neighbourhood_Dec29.json', 'r' ) as f :
     data = f.read()
